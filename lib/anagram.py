aae65f27b441c078ffae4de510653523b85cd3de
Jeff Mbithi <jeffmbithi9@gmail.com>
2025-05-20 19:13:10 +0300

    lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(self.word)
+
+    def match(self, word_list):
+        return [candidate for candidate in word_list
+            if candidate.lower() != self.word and sorted(candidate.lower()) == self.sorted_word]
